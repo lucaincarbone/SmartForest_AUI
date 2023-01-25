ccf159b3413e5e21e535e188c0b1998ea70a6bd7
Matteo <10575137@polimi.it>
2023-01-25 10:01:21 +0100

    added update api

---
+++ smartforest/server/api/update.post.ts
@@ -0,0 +1,16 @@
+import {Machine} from "../state_machine/Machine"
+
+const machine = Machine.Instance;
+/**
+ * Api handling the dialogflow exchange:
+ * Event is equivalent to HTTP POST request with body a json like this {phrase:"hi,forest!"}
+ * I read the phrase and ask the machine to handle: 1) The DialogFlow detect intent
+ *                                                  2) The creation of the Json to send back
+ *                                                  3) The advancement of the machine state
+ */
+export default defineEventHandler(async (event) => {
+    let answer = {a:""}
+    const body = await readBody(event)
+
+    return answer
+})
