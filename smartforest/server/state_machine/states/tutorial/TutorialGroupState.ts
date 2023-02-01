9325b7068798e9df52bf05f7befccabc435cf61a
Matteo <10575137@polimi.it>
2023-02-01 15:11:24 +0100

    small change to text

--- smartforest/server/state_machine/states/tutorial/TutorialGroupState.ts
+++ smartforest/server/state_machine/states/tutorial/TutorialGroupState.ts
@@ -52,7 +52,7 @@ export class TutorialGroupState extends TutorialState {
             answer.removed = super.getAllPossibleTutorialTrees()
             response.data = answer;
             response.success = true;
-            response.queryResult.fulfillmentText = "Congratulations, you succesfully grouped your plants. The tutorial is now over"
+            response.queryResult.fulfillmentText = "Congratulations, you succesfully grouped your plants. The tutorial is now over, you will be hable to replay it every time you want just by saying: i need the tutorial"
             return response
         } catch (e) {
             response.data = {
